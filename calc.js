e4c8482006390c49ce0c5f175674fa6a9ccb35e9
PavaniVutukuri <138871847+PavaniVutukuri@users.noreply.github.com>
2023-07-11 17:29:23 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,109 @@
+var clickSound1=new Audio()
+var clickSound2=new Audio()
+var clickSound3=new Audio()
+var clickSound4=new Audio()
+var clickSound5=new Audio()
+var clickSound6=new Audio()
+var clickSound7=new Audio()
+var clickSound8=new Audio()
+var clickSound9=new Audio()
+var clickSound10=new Audio()
+var clickSound11=new Audio()
+var clickSound12=new Audio()
+var clickSound13=new Audio()
+var clickSound14=new Audio()
+var clickSound15=new Audio()
+var clickSound16=new Audio()
+var clickSound20=new Audio()
+var clickSound21=new Audio()
+clickSound1.src="./01.mp3"
+clickSound3.src="./02.mp3"
+clickSound4.src="./03.mp3"
+clickSound5.src="./04.mp3"
+clickSound6.src="./05.mp3"
+clickSound7.src="./06.mp3"
+clickSound8.src="./07.mp3"
+clickSound9.src="./08.mp3"
+clickSound10.src="./09.mp3"
+clickSound11.src="./plus.mp3"
+clickSound12.src="./minus.mp3"
+clickSound13.src="./divis.mp3"
+clickSound14.src="./mod.mp3"
+clickSound15.src="./multi.mp3"
+clickSound16.src="./clear.mp3"
+clickSound2.src="./equal.mp3"
+clickSound20.src="./05.mp3"
+clickSound21.src="./01.mp3"
+
+function clickAudio1()
+{
+    clickSound1.play()
+}
+function clickAudio2()
+{
+    clickSound2.play()
+}
+function clickAudio3()
+{
+    clickSound3.play()
+}
+function clickAudioplus()
+{
+    clickSound11.play()
+}
+function clickAudio4()
+{
+    clickSound4.play()
+}
+function clickAudio5()
+{
+    clickSound5.play()
+}
+function clickAudio6()
+{
+    clickSound6.play()
+}
+function clickAudiominus()
+{
+    clickSound12.play()
+}
+function clickAudio7()
+{
+    clickSound7.play()
+}
+function clickAudio8()
+{
+    clickSound8.play()
+}
+function clickAudio9()
+{
+    clickSound9.play()
+}
+function clickAudiomulti()
+{
+    clickSound15.play()
+}
+function clickAudiomod()
+{
+    clickSound14.play()
+}
+function clickAudiozero()
+{
+    clickSound20.play()
+}
+function clickAudiodivis()
+{
+    clickSound13.play()
+}
+function clickAudioclear()
+{
+    clickSound16.play()
+}
+function clickAudiopoint()
+{
+    clickSound21.play()
+}
+function clickAudioequal()
+{
+    clickSound2.play()
+}
